62c97e94ef2c35d4495238f0838c1a32ccf0e19b
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-11 01:30:56 +0630

    addEnv

--- MessageBox/settings.py
+++ MessageBox/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j#$bb!3i)vm4gq$ds-ugy8)(lo2vtp2y(2lrebh83-d@_)8!c5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
