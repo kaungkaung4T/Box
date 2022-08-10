c18f0ca04003872bf1dc6010f3384aa079eaab51
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-11 00:51:36 +0630

    update

--- display/url.py
+++ display/url.py
@@ -36,7 +36,7 @@ urlpatterns = [
 
 
     path("api", views.api),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
